671024f332cb4906c0f1cc46b2b53faa8967bbc3
pdvrostovSPb <pdvrostov@mail.ru>
2022-06-12 21:08:31 +0300

    Add Example004_Div about div

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,6 @@
+﻿double numberA = new Random().Next();
+Console.WriteLine(numberA);
+double numberB = new Random().Next();
+Console.WriteLine(numberB);
+double result = numberA / numberB;
+Console.WriteLine(result);
