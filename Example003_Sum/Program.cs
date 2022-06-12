9aa96e9950ec14913ac45cd73f8e335ab0f0b6f8
pdvrostovSPb <pdvrostov@mail.ru>
2022-06-12 20:36:10 +0300

    Add Example003_Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,7 @@
+﻿int numberA = new Random().Next(1, 101);// 1,2,3...99,100
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 101);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
+
