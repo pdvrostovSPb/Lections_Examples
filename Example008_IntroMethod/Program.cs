fe3631cf54174603fb2643d859c6f428541d7558
pdvrostovSPb <pdvrostov@mail.ru>
2022-06-15 19:48:48 +0300

    add the first past Example008_IntroMethod

---
+++ Example008_IntroMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿/* int a1 = 78;
+int b1 = 44;
+int c1 = 44;
+
+
+int a2 = 99;
+int b2 = 5644;
+int c2 = 41;
+
+int a3 = 7;
+int b3 = 4;
+int c3 = 454;
+
+int max = a1;
+if(b1 > max) max = b1;
+if(c1 > max) max = c1;
+
+if(a2 > max) max = a2;
+if(b2 > max) max = b2;
+if(c2 > max) max = c2;
+
+if(a3 > max) max = a3;
+if(b3 > max) max = b3;
+if(c3 > max) max = c3;
+
+Console.WriteLine(max); */
