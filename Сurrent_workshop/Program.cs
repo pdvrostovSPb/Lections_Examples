eee6dd43a474ea70eb2781636d0d531a095198c1
pdvrostovSPb <pdvrostov@mail.ru>
2022-06-16 23:35:00 +0300

    added Program.cs to Current_workshop

---
+++ Сurrent_workshop/Program.cs
@@ -0,0 +1,66 @@
+﻿/* /* Напишите программу, которая
+принимает на вход координаты двух точек и
+находит расстояние между ними в 2D
+пространстве.
+ */
+
+/*  Console.WriteLine("введите координату X1");
+ int X1 = int.Parse(Console.ReadLine());
+
+ Console.WriteLine("введите координату Y1");
+ int Y1 = int.Parse(Console.ReadLine());
+
+  Console.WriteLine("введите координату X2");
+ int X2 = int.Parse(Console.ReadLine());
+
+ Console.WriteLine("введите координату Y2");
+ int Y2 = int.Parse(Console.ReadLine());
+
+double result = Math.Sqrt (Math.Pow (X1 -X2,2) + Math.Pow (Y1 - Y2,2));
+
+Console.WriteLine (Math.Round (result, 3)); // 3 после result кол-во знаков после запятой
+ */
+/* Напишите программу, которая
+принимает на вход число (N) и выдаёт таблицу
+квадратов чисел от 1 до N. */
+
+/* Console.WriteLine("введите число ");
+int N = int.Parse(Console.ReadLine());
+
+int Count = 1;
+
+while (Count <= N)
+{
+    Console.Write(Math.Pow(Count, 2) + " ");
+    Count++;
+} */
+
+/*  Напишите программу, которая принимает на
+вход пятизначное число и проверяет, является ли оно
+палиндромом. */
+
+/* Напишите программу, которая принимает на
+вход координаты двух точек и находит расстояние между
+ними в 3D пространстве. */
+
+int[] array = new int[8];
+
+int i = 0;
+
+while (i < array.Length)
+{
+    Console.Write(array[i] = new Random().Next(0,2));
+    i++;
+}
+Console.WriteLine();
+
+void PrintArray(int[] col)
+{
+    for (int i = 0; i < mass.Length; i++)
+    {
+         Console.Write(mass[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(mass);
