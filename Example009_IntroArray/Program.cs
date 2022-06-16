60d057b068bbac792d3bd9315ef6076b725fcbfa
pdvrostovSPb <pdvrostov@mail.ru>
2022-06-16 14:41:02 +0300

    added and finished Example008_IntroMethod and Example009_IntroArray

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Найти максимум из 9 чисел*/
+
+int[] array = { 11, 21, 13, 14, 51, 19, 17, 81, 91 };
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int max = Max(Max(array[0], array[1], array[2]),
+          Max(array[3], array[4], array[5]),
+          Max(array[6], array[7], array[8])
+);
+Console.WriteLine(max);
